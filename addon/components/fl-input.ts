918c0ba6823b58b3933d0200236209a36a6eeef8
Bill Dami <billdami@gmail.com>
2020-04-01 13:29:31 -0400

    start converting to typescript and glimmer components

---
+++ addon/components/fl-input.ts
@@ -0,0 +1,106 @@
+import Component from '@glimmer/component';
+import { tracked } from '@glimmer/tracking';
+import { action } from '@ember/object';
+import { isEmpty } from '@ember/utils';
+import { scheduleOnce } from '@ember/runloop';
+import ObjectProxy from '@ember/object/proxy';
+import ArrayProxy from '@ember/array/proxy';
+
+export interface FlInputArgs {
+    value: any;
+    placeholder?: string;
+    containerClass?: string;
+    inputBaseClass?: string;
+}
+
+export default class FlInput extends Component<FlInputArgs> {
+    @tracked hasFocus: boolean = false;
+
+    /**
+     * Returns true if the provided value argument is not empty
+     */
+    get hasContent(): boolean {
+        return !this.isValueEmpty(this.args.value);
+    }
+
+    /**
+     * Returns true if the component is either focused or has a non-empty value
+     */
+    get hasFocusOrContent(): boolean {
+        return this.hasContent || this.hasFocus;
+    }
+
+    /**
+     * The base class applied to all inputs, defaults to the Bootstrap form-control class
+     */
+    get inputBaseClass(): string {
+        return this.args.inputBaseClass ?? 'form-control';
+    }
+
+    /**
+     * Updates the component's focused state when it recieves focus
+     */
+    @action
+    onFocusIn() {
+        scheduleOnce('afterRender', this, () => this.setFocus(true));
+        //TODO see if we can do this with on modifiers
+        // tryInvoke(this, 'focus-in', ...arguments);
+    }
+
+    /**
+     * Updates the component's focused state when it loses focus
+     */
+    @action
+    onFocusOut() {
+        scheduleOnce('afterRender', this, () => this.setFocus(false));
+        //TODO see if we can do this with on modifiers
+        // tryInvoke(this, 'focus-out', ...arguments);
+    }
+
+    /**
+     * Safely sets the component's new focused state
+     * @param {Boolean} newValue
+     */
+    private setFocus(newValue: boolean) {
+        if(!this.isDestroying && !this.isDestroyed) {
+            this.hasFocus = newValue;
+        }
+    }
+
+    /**
+     * Checks if the provided value is "empty" or not, handling ember object/array proxies
+     * @param {any} value
+     */
+    private isValueEmpty(value: any): boolean {
+        if (value instanceof ObjectProxy || value instanceof ArrayProxy) {
+            //@ts-ignore we need to use `.get()` to access the proxy value correctly
+            return this.isValueEmpty(value.get('content'));
+        }
+
+        return isEmpty(value);
+    }
+
+    //TODO probably dont need these actions...
+    //but test using `on` modifiers on the component externally
+    //e.g. `<FlInput {{on "keyPress" this.onKeyPress}} />
+    // enter () {
+    //     tryInvoke(this, 'enter', ...arguments);
+    // },
+
+    // insertLine () {
+    //     tryInvoke(this, 'insert-newline', ...arguments);
+    // },
+
+    // escapePress () {
+    //     tryInvoke(this, 'escape-press', ...arguments);
+    // },
+
+    // keyPress () {
+    //     tryInvoke(this, 'key-press', ...arguments);
+    // },
+
+    // keyUp () {
+    //     tryInvoke(this, 'key-up', ...arguments);
+    // }
+}
+
--- addon/components/fl-select.ts
+++ addon/components/fl-select.ts
@@ -1,12 +1,10 @@
 import Component from '@ember/component';
 import FloatingLabelMixin from '../mixins/floating-label';
-import layout from '../templates/components/fl-select';
 import { tryInvoke } from '@ember/utils';
 import { alias } from '@ember/object/computed';
 import { not } from '@ember/object/computed';
 
 export default Component.extend(FloatingLabelMixin, {
-    layout,
     classNames: ['floating-label-select'],
     valuePath: "value",
     labelPath: "label",
