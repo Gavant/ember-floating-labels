37f153cd015525ff39df3ac487260d41fcff7a1d
Bill Dami <billdami@gmail.com>
2020-04-02 12:24:14 -0400

    fix tests

--- tests/integration/components/fl-select-test.ts
+++ tests/integration/components/fl-select-test.ts
@@ -1,15 +1,17 @@
-import { moduleForComponent, test } from 'ember-qunit';
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { render } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
 
-moduleForComponent('fl-select', 'Integration | Component | fl select', {
-  integration: true
-});
+module('Integration | Component | fl-select', function(hooks) {
+    setupRenderingTest(hooks);
 
-test('it renders', function(assert) {
-  // Set any properties with this.set('myProperty', 'value');
-  // Handle any actions with this.on('myAction', function(val) { ... });
+    test('it renders', async function(assert) {
+        // Set any properties with this.set('myProperty', 'value');
+        // Handle any actions with this.set('myAction', function(val) { ... });
 
-  this.render(hbs`{{fl-select}}`);
+        await render(hbs`<FlSelect />`);
 
-  assert.equal(this.$().text().trim(), '');
+        assert.equal(this.element.textContent?.trim(), '');
+    });
 });
--- tests/integration/components/fl-textarea-test.ts
+++ tests/integration/components/fl-textarea-test.ts
@@ -1,24 +1,17 @@
-import { moduleForComponent, test } from 'ember-qunit';
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { render } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
 
-moduleForComponent('fl-textarea', 'Integration | Component | fl textarea', {
-  integration: true
-});
+module('Integration | Component | fl-textarea', function(hooks) {
+    setupRenderingTest(hooks);
 
-test('it renders', function(assert) {
-  // Set any properties with this.set('myProperty', 'value');
-  // Handle any actions with this.on('myAction', function(val) { ... });
+    test('it renders', async function(assert) {
+        // Set any properties with this.set('myProperty', 'value');
+        // Handle any actions with this.set('myAction', function(val) { ... });
 
-  this.render(hbs`{{fl-textarea}}`);
+        await render(hbs`<FlTextarea />`);
 
-  assert.equal(this.$().text().trim(), '');
-
-  // Template block usage:
-  this.render(hbs`
-    {{#fl-textarea}}
-      template block text
-    {{/fl-textarea}}
-  `);
-
-  assert.equal(this.$().text().trim(), 'template block text');
+        assert.equal(this.element.textContent?.trim(), '');
+    });
 });
