65ab2816b36dda14a65286d8c0b6201cd02d7ceb
Bill Dami <billdami@gmail.com>
2020-04-02 11:25:40 -0400

    styling fixes for FlSelect

--- tests/dummy/app/routes/select.js
+++ tests/dummy/app/routes/select.js
@@ -3,6 +3,6 @@ import Route from '@ember/routing/route';
 export default class SelectRoute extends Route {
     setupController(controller) {
         super.setupController(...arguments);
-        controller.selectLgValue = controller.customSelectOptions.lastObject;
+        controller.selectLgValue = controller.customSelectOptions.lastObject.abbrv;
     }
 }
