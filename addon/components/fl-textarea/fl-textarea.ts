0f1ae5014fc90539576439ea354306e106d5aeb6
Adam Baker <bakerac4@gmail.com>
2022-07-08 10:35:36 -0400

    Work on v2 conversion

--- addon/components/fl-textarea/fl-textarea.ts
+++
@@ -1,5 +0,0 @@
-import FlInput, { FlInputArgs } from '@gavant/ember-floating-labels/components/fl-input/fl-input';
-
-export interface FlTextareaArgs extends FlInputArgs {}
-
-export default class FlTextarea extends FlInput<FlTextareaArgs> {}
--- app/components/fl-textarea.js
+++
@@ -1,1 +0,0 @@
-export { default } from '@gavant/ember-floating-labels/components/fl-textarea/fl-textarea';
